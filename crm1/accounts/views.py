5359b50af77902c45755be54d475027d550ce838
Nguyen Minh Phuc <chilavisaoss@gmail.com>
2022-04-24 11:19:53 +0700

    Dynamic URL routing and rendering data

--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,14 +1,40 @@
 from django.shortcuts import render
-
-
 # Create your views here.
+from .models import *
+
+
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+
+    total_customers = customers.count()
+
+    total_orders = orders.count()
+    num_orders_pending = orders.filter(status='Pending').count()
+    num_orders_delivered = orders.filter(status='Delivered').count()
+
+    context = {
+        'orders': orders,
+        'customers': customers,
+        'total_customers': total_customers,
+        'total_orders': total_orders,
+        'num_orders_pending': num_orders_pending,
+        'num_orders_delivered': num_orders_delivered
+    }
+
+    return render(request, 'accounts/dashboard.html', context=context)
 
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products_list = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products_list': products_list})
 
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request, primary_key):
+    id_customer = Customer.objects.get(id=primary_key)
+    orders = id_customer.order_set.all()
+    num_orders = orders.count()
+
+    context = {'id_customer': id_customer, 'orders': orders, 'num_orders': num_orders}
+
+    return render(request, 'accounts/customer.html', context=context)
